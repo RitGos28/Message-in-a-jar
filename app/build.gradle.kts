0fe29d1cc55322084e4680742c77beea947e3047
Ritwik-the-programmer <gossainritwik@gmail.com>
2026-07-11 00:37:00 +0530

    Changed timezone detection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ritwikg.messageinajar"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.8"
+        versionCode = 12
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
